3a955cd5ef4cc5f4dd7433c8ac8f18c00015091e
Milan Krunic <milankrunic12@gmail.com>
2021-06-09 13:05:26 +0200

    Navigation of the pages

---
+++ Trening/app/src/main/java/com/example/trening/RamenaIRuke.java
@@ -0,0 +1,14 @@
+package com.example.trening;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RamenaIRuke extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ramena_iruke);
+    }
+}
