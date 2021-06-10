0c69e653a319cdd8a3b14e5675552659e64446e6
Milan Krunic <milankrunic12@gmail.com>
2021-06-10 17:19:08 +0200

    Added choice betwen mens and womens training plan

--- Trening/app/src/main/java/com/example/trening/MainActivity.java
+++ Trening/app/src/main/java/com/example/trening/MainActivity.java
@@ -14,55 +14,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button1 = (Button) findViewById(R.id.trening1Button);
+        button1=(Button) findViewById(R.id.buttonMuskarci);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                otvoriTreningPrsa();
-
+                otvoriMuskarce();
             }
         });
 
-        button2 = (Button) findViewById(R.id.trening2Button);
+        button2=(Button) findViewById(R.id.buttonZene);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                otvoriTreningNoge();
+                otvoriZene();
             }
         });
 
-        button3 = (Button) findViewById(R.id.trening3Button);
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                otvoriTreningLedja();
-            }
-        });
-
-        button4 = (Button) findViewById(R.id.trening4Button);
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                otvoriTreningRamena();
-            }
-        });
-
-
     }
-    public void otvoriTreningPrsa(){
-        Intent intent =new Intent(this,PrsaITriceps.class);
+    public void otvoriMuskarce(){
+        Intent intent=new Intent(this,Muskarci.class);
         startActivity(intent);
     }
-    public void otvoriTreningNoge(){
-        Intent intent =new Intent(this,NogeITrbusnaci.class);
-        startActivity(intent);
-    }
-    public void otvoriTreningLedja(){
-        Intent intent =new Intent(this,LedjaIBiceps.class);
-        startActivity(intent);
-    }
-    public void otvoriTreningRamena(){
-        Intent intent =new Intent(this,RamenaIRuke.class);
+    public void otvoriZene(){
+        Intent intent=new Intent(this,Zene.class);
         startActivity(intent);
     }
 }
---
+++ Trening/app/src/main/java/com/example/trening/Muskarci.java
@@ -0,0 +1,66 @@
+package com.example.trening;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Muskarci extends AppCompatActivity {
+Button button1,button2,button3,button4;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_muskarci);
+        button1 = (Button) findViewById(R.id.trening1Button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                otvoriTreningPrsa();
+
+            }
+        });
+
+        button2 = (Button) findViewById(R.id.trening2Button);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                otvoriTreningNoge();
+            }
+        });
+
+        button3 = (Button) findViewById(R.id.trening3Button);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                otvoriTreningLedja();
+            }
+        });
+
+        button4 = (Button) findViewById(R.id.trening4Button);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                otvoriTreningRamena();
+            }
+        });
+
+    }
+    public void otvoriTreningPrsa(){
+        Intent intent =new Intent(this,PrsaITriceps.class);
+        startActivity(intent);
+    }
+    public void otvoriTreningNoge(){
+        Intent intent =new Intent(this,NogeITrbusnaci.class);
+        startActivity(intent);
+    }
+    public void otvoriTreningLedja(){
+        Intent intent =new Intent(this,LedjaIBiceps.class);
+        startActivity(intent);
+    }
+    public void otvoriTreningRamena(){
+        Intent intent =new Intent(this,RamenaIRuke.class);
+        startActivity(intent);
+    }
+}
